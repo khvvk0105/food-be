30eae317ba36fb34f5f4d1a207924f71831d34b2
Khurelbaatar Dorj <23LP5779@G304-CC0083.local>
2024-04-05 10:02:53 +0800

    food

---
+++ src/models/food.schema.ts
@@ -0,0 +1,6 @@
+import mongoose, { model, Schema } from "mongoose";
+const FoodSchema = new Schema({
+  name: String,
+  categoryId: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+});
+export const FoodModel = mongoose.models.Food || model("Food", FoodSchema);
